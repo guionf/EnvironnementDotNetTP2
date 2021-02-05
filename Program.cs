98222998f6e0ecb6332a32b7b651039dfee8c4de
Guillaume ONFRAY <guillaume.onfray@epsi.fr>
2021-02-05 14:00:11 +0100

    Premier commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2.Entities;
+using TP2.Utils;
+
+namespace TP2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var item in FakeDb.Instance.Users)
+            {
+                Console.WriteLine(item);
+            }
+
+            #region Q1
+            Console.WriteLine("Question 1");
+            // Afficher le nombre de personne s'appelant Dupond ou Dupont.
+
+            #endregion
+            #region Q2
+            Console.WriteLine("Question 2");
+            // Afficher les personnes enregistré avec le role Automobiliste.
+
+            #endregion
+            #region Q3
+            Console.WriteLine("Question 3");
+            // Afficher les plaques d'immatriculation de toutes les voitures (une seule fois par voiture) liées à au moins un utilisateur.
+
+            #endregion
+            #region Q4
+            Console.WriteLine("Question 4");
+            // Afficher la ou les voiture(s) avec le plus de kilomètre
+
+            #endregion
+            #region Q5
+            Console.WriteLine("Question 5");
+            // Afficher le classement des types de voiture par nombre de voiture unique présentes du plus grand au plus petit.
+
+            #endregion
+            #region Q6
+            Console.WriteLine("Question 6");
+            // Afficher les "Garagiste" liés à 4 voitures ou plus.
+
+            #endregion
+            #region Q7
+            Console.WriteLine("Question 7");
+            // Afficher les "Controlleur" et la liste des voitures aux quelles ils sont liés.
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
